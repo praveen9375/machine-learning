f97bf80d989e50ef3bbc0bb608030421dde3deb7
praveen9375 <48412124+praveen9375@users.noreply.github.com>
2021-05-18 13:56:06 +0545

    Create np_zeros.py

---
+++ np_zeros.py
@@ -0,0 +1,4 @@
+#We'll create an array of 5 elements, all of which are 0. We'll also create an array with the same shape as transposed, but containing only 1 as the elements.
+import numpy as np
+zeros_arr = np.zeros(5)
+ones_arr = np.ones_like(transposed)
