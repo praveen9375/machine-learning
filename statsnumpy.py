bdc73b0132447084bbb8cdf274b4471c21f70601
praveen9375 <48412124+praveen9375@users.noreply.github.com>
2021-05-18 14:20:28 +0545

    Create statsnumpy.py

---
+++ statsnumpy.py
@@ -0,0 +1,5 @@
+#The first function to complete is get_min_max, which returns the overall minimum and maximum element in data.
+def get_min_max(data):
+  overall_min = data.min()
+  overall_max = data.max()
+  return overall_min, overall_max
